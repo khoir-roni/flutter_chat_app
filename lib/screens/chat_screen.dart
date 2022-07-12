a2be14301d44b99e07c8693a399bc69bb9d8ed70
khoir-roni <khoir.roni@gmail.com>
2022-07-12 21:04:30 +0700

    rendering stream data with stream builder

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -7,53 +7,37 @@ class ChatScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final firestore = FirebaseFirestore.instance;
     return Scaffold(
       body: StreamBuilder(
-          stream: firestore.collection('testing').snapshots(),
+          stream: FirebaseFirestore.instance
+              .collection("chats/jo6fgCwQ2VbQcYGN9UIl/messages")
+              .snapshots(),
           builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (!snapshot.hasData) return const SizedBox.shrink();
+            // if (!snapshot.hasData) return const SizedBox.shrink();
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            final documents = snapshot.data.docs;
             return ListView.builder(
-              itemCount: snapshot.data.docs.length,
+              itemCount: documents.length,
               itemBuilder: (BuildContext context, int index) {
-                final docData = snapshot.data.docs[index].data();
-                final dateTime = (docData['timestamp'] as Timestamp).toDate();
-                return ListTile(
-                  title: Text(dateTime.toString()),
+                return Container(
+                  padding: EdgeInsets.all(8),
+                  child: Text(documents[index]["text"]),
                 );
+                // final docData = snapshot.data.docs[index].data();
+                // final dateTime = (docData['timestamp'] as Timestamp).toDate();
+                // return ListTile(
+                //   title: Text(dateTime.toString()),
+                // );
               },
             );
           }),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          FirebaseFirestore.instance
-              .collection("chats/jo6fgCwQ2VbQcYGN9UIl/messages")
-              .snapshots()
-              .listen((data) {
-            data.docs.forEach((element) {
-              print(element["text"]);
-            });
-            // print(data.docs[0]["text"]);
-            // final textData = [];
-            // for (var doc in event.docs) {
-            //   textData.add(doc.data()["text"]);
-            // }
-            // print("cities in CA: ${textData.join(", \n")}");
-          });
-          // firestore
-          //     .collection("testing")
-          //     // .where("state", isEqualTo: "CA")
-          //     .snapshots()
-          //     .listen((event) {
-          //   final docData = [];
-          //   for (var doc in event.docs) {
-          //     final data = (doc.data()["timestamp"] as Timestamp).toDate();
-          //     docData.add(data.toString());
-          //   }
-          //   print("timestamp1: ${docData.join(", \n")}");
-          // });
-        },
-        child: Icon(Icons.add),
+        onPressed: () {},
+        child: const Icon(Icons.add),
       ),
     );
   }
