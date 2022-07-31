69fdf59ae96653ed8e5591cab84e00856eb70642
khoir-roni <khoir.roni@gmail.com>
2022-07-31 13:25:40 +0700

    improving image uploud

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -43,6 +43,7 @@ class _AuthScreenState extends State<AuthScreen> {
             .child('user_image')
             .child('${userCredential.user.uid}.jpg');
         await ref.putFile(image);
+        final url = await ref.getDownloadURL();
 
         await FirebaseFirestore.instance
             .collection('users')
@@ -50,6 +51,7 @@ class _AuthScreenState extends State<AuthScreen> {
             .set({
           'username': userName,
           'email': email,
+          'image_url': url,
         });
       }
     } on FirebaseException catch (err) {
