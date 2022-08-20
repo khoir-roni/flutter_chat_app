bb6864eff3f1284e3aab203fe6a3d8c21fe617e6
khoir-roni <khoir.roni@gmail.com>
2022-08-20 20:50:57 +0700

    firebase cloud functions init

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,16 @@
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
 // exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
+//   functions.logger.info("Hello logs!", { structuredData: true });
 //   response.send("Hello from Firebase!");
 // });
+
+exports.createUser = functions.firestore
+    .document("chat/{message}")
+    .onCreate((snap, _context) => {
+      console.log(snap.data());
+      return;
+    });
